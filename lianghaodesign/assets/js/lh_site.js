29b01431e26a9ca45c4d53d8ab908de026b545e6
chennanfei <chennanfei2006@163.com>
2014-04-12 11:47:53 +0800

    Added a function: when window scrolls, sync up the status of menu items.

--- lianghaodesign/assets/js/lh_site.js
+++ lianghaodesign/assets/js/lh_site.js
@@ -1,8 +1,126 @@
-TM.declare('lh.controller.SharedController').inherit('thinkmvc.Controller').extend({
+TM.declare('lh.controller.BaseController').inherit('thinkmvc.Controller').extend({
+  preventedActions:{},
+  slideTimeSlow:500,
+  slideTimeNormal:300,
+  selectedClass:'background-stress',
+
+  preventDoubleAction:function(action) {
+    var self = this;
+    if (!self.preventedActions[action]) {
+      self.preventedActions[action]= true;
+
+      setTimeout(function() {
+        self.preventedActions[action] = false;
+      }, self.slideTimeSlow);
+
+      return false;
+    }
+    return true;
+  }
+});
+
+TM.declare('lh.controller.ItemMenuController').inherit('lh.controller.BaseController').extend((function(){
+  function expandSubMenu($menuItem) {
+    // When clicking menu items, the window will scroll. prevent this function being executed twice in short time.
+    if (this.preventDoubleAction('window-scroll') || $menuItem.hasClass(this.selectedClass)) {
+      return;
+    }
+
+    var el = this._el, slideTime = this.slideTimeNormal,
+      selected = this.selectedClass, selectedEl = '.' + selected,
+      $selectItem = el.$menuItems.filter(selectedEl).removeClass(selected);
+    if ($selectItem.length) {
+      $selectItem.siblings('.tm-sub-list').slideUp(slideTime);
+      el.$subMenuItems.filter(selectedEl).removeClass(selected);
+    }
+    $menuItem.addClass(selected);
+    $menuItem.siblings('.tm-sub-list').slideDown(slideTime);
+  }
+
+  function scrollContentToTop($menuItem) {
+    var mainTop = this._el.$mainContent.offset().top,
+      offset = $('#' + $menuItem.data('itemId')).offset();
+    $('html,body').animate({scrollTop: offset.top - mainTop}, this.slideTimeNormal);
+  }
+
+  return {
+    events:{
+      'click .close-btn':'close',
+      'click .menu-item':'renderSubMenu',
+      'click .sub-menu-item':'renderItemContent',
+      'scroll window':'updateMenu'
+    },
+
+    selectors:{
+      designList: '.design-list',
+      itemContents:'.design-list .content',
+      mainContent: '#mainContent',
+      menuItems:'#menu .menu-item',
+      subMenuItems:'#menu .sub-menu-item'
+    },
+
+    close:function(event) {
+      var el = this._el, $target = $(event.currentTarget), selected = this.selectedClass, selectedEl = '.' + selected;
+      $target.parents('.content').fadeOut(this.slideTimeNormal, function() {
+        el.$subMenuItems.filter(selectedEl).removeClass(selected);
+        $target.parents('.design-list').find('.preview-content').show();
+      });
+    },
+
+    renderSubMenu:function(event) {
+      var $target = $(event.currentTarget), self = this;
+      if ($target.hasClass(this.selectedClass)) {
+        return;
+      }
+
+      // expand sub menu
+      expandSubMenu.call(this, $target);
+
+      // scroll the block to top
+      scrollContentToTop.call(this, $target);
+    },
+
+    renderItemContent:function(event) {
+      var $target = $(event.currentTarget), contentId = $target.data('contentId'),
+        selected = this.selectedClass, selectedEl = '.' + selected;
+      if (!contentId || $target.hasClass(selected)) {
+        return;
+      }
+
+      var $content = $('#' + contentId);
+      if (!$content.length) {
+        return;
+      }
+
+      this._el.$subMenuItems.filter(selectedEl).removeClass(selected);
+      $target.addClass(selected);
+
+      this._el.$itemContents.filter(':visible').hide();
+      $content.fadeIn(this.slideTimeNormal);
+    },
+
+    /*
+    * When window scrolls, update left menu according to first visible content
+    * */
+    updateMenu:function() {
+      var el = this._el, $list = el.$designList, $win = $(window), firstId,
+        mainTop = el.$mainContent.offset().top;
+      $list.each(function(index, el) {
+        var $el = $(el), contentOffset = $el.offset().top + $el.height() - 100;
+        if (contentOffset > $win.scrollTop() + mainTop) {
+          firstId = $el.attr('id');
+          return false;
+        }
+      });
+
+      var $menuItem = el.$menuItems.filter('[data-item-id=' + firstId + ']');
+      expandSubMenu.call(this, $menuItem);
+    }
+  };
+})());
+
+TM.declare('lh.controller.PageController').inherit('lh.controller.BaseController').extend({
   events:{
-    'click .close-btn':'closeContent',
-    'click .menu-item':'renderSubMenu',
-    'click .sub-menu-item':'renderItemContent',
     'mouseenter .close-btn':'toggleCloseBtn',
     'mouseenter #footMarker':'toggleFooter',
     'mouseleave .close-btn':'toggleCloseBtn',
@@ -11,64 +129,8 @@ TM.declare('lh.controller.SharedController').inherit('thinkmvc.Controller').exte
   },
 
   selectors:{
-    firstList: '.design-list:first',
     footer:'#footer',
-    itemContents:'.design-list .content',
-    menuItems:'#menu .menu-item',
-    realFooter:'#realFooter',
-    subMenuItems:'#menu .sub-menu-item'
-  },
-
-  slideTimeSlow:500,
-  slideTimeNormal:300,
-  selectedClass:'background-stress',
-
-  closeContent:function(event) {
-    var el = this._el, $target = $(event.currentTarget), selected = this.selectedClass, selectedEl = '.' + selected;
-    $target.parents('.content').fadeOut(this.slideTimeNormal, function() {
-      el.$subMenuItems.filter(selectedEl).removeClass(selected);
-      $target.parents('.design-list').find('.preview-content').show();
-    });
-  },
-
-  renderSubMenu:function(event) {
-    var $target = $(event.currentTarget), selected = this.selectedClass, selectedEl = '.' + selected;
-    if ($target.hasClass(selected)) {
-      return;
-    }
-
-    var el = this._el, slideTime = this.slideTimeNormal,
-      $selectItem = el.$menuItems.filter(selectedEl).removeClass(selected);
-    if ($selectItem.length) {
-      $selectItem.siblings('.tm-sub-list').slideUp(slideTime);
-      el.$subMenuItems.filter(selectedEl).removeClass(selected);
-    }
-    $target.addClass(selected);
-    $target.siblings('.tm-sub-list').slideDown(slideTime);
-
-    // scroll the block to top
-    var firstTop = el.$firstList.offset().top,
-      offset = $('#' + $target.data('itemId')).offset();
-    $('html body').animate({scrollTop: offset.top - firstTop}, 500);
-  },
-
-  renderItemContent:function(event) {
-    var $target = $(event.currentTarget), contentId = $target.data('contentId'),
-      selected = this.selectedClass, selectedEl = '.' + selected;
-    if (!contentId || $target.hasClass(selected)) {
-      return;
-    }
-
-    var $content = $('#' + contentId);
-    if (!$content.length) {
-      return;
-    }
-
-    this._el.$subMenuItems.filter(selectedEl).removeClass(selected);
-    $target.addClass(selected);
-
-    this._el.$itemContents.filter(':visible').hide();
-    $content.fadeIn(500);
+    realFooter:'#realFooter'
   },
 
   toggleCloseBtn:function(event) {
@@ -100,7 +162,7 @@ TM.declare('lh.controller.SharedController').inherit('thinkmvc.Controller').exte
   }
 });
 
-TM.declare('lh.controller.LoadingController').inherit('thinkmvc.Controller').extend(function() {
+TM.declare('lh.controller.LoadingController').inherit('lh.controller.BaseController').extend(function() {
   // private functions
   function initImageList() {
     var $images, el = this._el;
@@ -155,21 +217,23 @@ TM.declare('lh.controller.LoadingController').inherit('thinkmvc.Controller').ext
     },
 
     initialize:function() {
-      this.invoke('thinkmvc.Controller:initialize');
+      this.invoke('lh.controller.BaseController:initialize');
       initImageList.call(this);
       switchShowImages.call(this);
     }
   };
 });
 
-TM.declare('lh.controller.WorksController').inherit('thinkmvc.Controller').extend({
+TM.declare('lh.controller.WorksController').inherit('lh.controller.BaseController').extend({
   initialize:function() {
-    this.U.createInstance('lh.controller.SharedController');
+    this.U.createInstance('lh.controller.PageController');
+    this.U.createInstance('lh.controller.ItemMenuController');
   }
 });
 
-TM.declare('lh.controller.LifeController').inherit('thinkmvc.Controller').extend({
+TM.declare('lh.controller.LifeController').inherit('lh.controller.BaseController').extend({
   initialize:function() {
     this.U.createInstance('lh.controller.SharedController');
+    this.U.createInstance('lh.controller.ItemMenuController');
   }
 });
