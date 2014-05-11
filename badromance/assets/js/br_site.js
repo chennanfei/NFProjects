4dccac29a9a763243ba4b8e199e04702f5131533
chennanfei <chennanfei2006@163.com>
2014-05-11 16:03:24 +0800

    Added shop section and its movement

--- badromance/assets/js/br_site.js
+++ badromance/assets/js/br_site.js
@@ -1,24 +1,59 @@
-TM.declare('br.controller.BaseController').inherit('thinkmvc.Controller').extend({
-  effectTime: {
-    TINY: 10,
-    FAST: 200,
-    NORMAL: 300,
-    SLOW: 500
-  },
+TM.declare('br.controller.BaseController').inherit('thinkmvc.Controller').extend(function() {
+  // private static variables
+  var lastPos, scrollTop;
 
-  getPosition: function() {
-    var $win = $(window), sceneHeight = $('#scene').height() - $win.height();
-    return $win.scrollTop() / sceneHeight * 190;
-  },
+  // private functions
+  function getMovedOffset() {
+    var lp = lastPos || 0;
+    lastPos = scrollTop;
+    return scrollTop - lp;
+  }
 
-  getMovedOffset: function() {
-    var pos = $(window).scrollTop(), lastPos = this.lastPos || 0;
-    this.lastPos = pos;
-    return pos - lastPos;
-  },
+  function getPosition() {
+    if (!this.scroll.RATIO) {
+      // compute the max distance the window can scrolls
+      var sceneHeight = $('#scene').height() - $(window).height();
+      if (isNaN(sceneHeight) || sceneHeight <= 0) {
+        throw new Error('The height of #scene is invalue.');
+      }
 
-  isPageUp: function() {
-    return this.getMovedOffset() >= 0;
+      // In this case, window triggers scroll event every time when it scrolls 100,
+      // scrollRatio will transform scrollTop to a smaller value so that elements' move
+      // can be controlled more precisely.
+      this.scroll.RATIO = sceneHeight / this.scroll.TRANSFORM;
+    }
+    return scrollTop / this.scroll.RATIO;
+  }
+
+  return  {
+    effectTime: {
+      TINY: 10,
+      FAST: 200,
+      NORMAL: 300,
+      SLOW: 500
+    },
+
+    scroll: {
+      MULTIPLE: 25,
+      RATIO: 0,
+      TRANSFORM: 190
+    },
+
+    initScrollStatus: function() {
+      var self = this, movedOffset = getMovedOffset.call(this), $win = $(window);
+      scrollTop = $win.scrollTop();
+
+      this._scrollStatus = {
+        isPageUp: movedOffset > 0,
+        position: getPosition.call(self),
+        scrollTop: scrollTop,
+        winWidth: $win.width()
+      };
+    },
+
+    getScrollStatus: function() {
+      return this._scrollStatus;
+    }
   }
 });
 
@@ -48,7 +83,7 @@ TM.declare('br.controller.MainController').inherit('br.controller.BaseController
       self._el.$scene.show();
 
       self.U.createInstance('br.controller.HomeController'); // DEBUG - Home
-      //self.U.createInstance('br.controller.ShopController');
+      self.U.createInstance('br.controller.ShopController');
     }, self.effectTime.FAST);
   },
 
@@ -67,29 +102,10 @@ TM.declare('br.controller.MainController').inherit('br.controller.BaseController
   }
 });
 
-TM.declare('br.controller.HomeController').inherit('br.controller.BaseController').extend({
-  events: {
-    'scroll window': 'handleScrolling'
-  },
+TM.declare('br.controller.HomeController').inherit('br.controller.BaseController').extend(function() {
+  function loadShow() {
+    var el = this._el, time = this.effectTime.SLOW;
 
-  selectors: {
-    head: '#home-head',
-    home: '#home',
-    foot: '#home-foot',
-    siteDesc: '#site-desc',
-    siteMenu: '#site-menu',
-    siteTitle: '#site-title'
-  },
-
-  markedPos: 60, // home section acts in [0, 1000]
-
-  initialize: function() {
-    this.invoke('br.controller.BaseController:initialize');
-    this.loadShow();
-  },
-
-  loadShow: function() {
-    var el = this._el, time = this.effectTime.SLOW, self = this;
     // show in order: home section, footer, header.
     el.$home.fadeIn(time, function() {
       el.$head.slideDown(time, function() {
@@ -97,78 +113,255 @@ TM.declare('br.controller.HomeController').inherit('br.controller.BaseController
       });
     });
 
-    el.$siteMenu.data('origin', parseInt(el.$siteMenu.css('left'), 10));
-    el.$siteDesc.data('origin', parseInt(el.$siteDesc.css('left'), 10));
-    el.$siteTitle.data('origin', parseInt(el.$siteTitle.css('left'), 10));
-  },
+    // save the original left value
+    [el.$siteMenu, el.$siteDesc, el.$siteTitle].forEach(function($el) {
+      $el.data('origin', parseInt($el.css('left'), 10));
+    });
+  }
 
-  handleScrolling: function() {
-    var pos = this.getPosition(), el = this._el, isPageUp = this.isPageUp(), multiple = 25;
-
-    //DEBUG
-    console.log('isPageUp:' + isPageUp + ', scrollTop:' + $(window).scrollTop() + ', pos:' + pos);
-
-    // site menu
-    if (pos < 32) {
-      var offset = pos * multiple;
-      el.$siteMenu.css('left', -1 * offset);
+  /*
+   * Move element by position
+   * @prams:
+   * config = {
+       $el: this._el.$foot, // element to move
+       cssProp: 'bottom', // css property to be modified so element looks moving
+       maxPos: 30, // element moves in a scope between minPos and maxPos
+       minPos: 19,
+       minOffset: 12 // when offset to the original position is less than this value,
+                      // force offset to 0 so the element can go back to its original position
+       };
+   * */
+  function move(config) {
+    var scrollStatus = this.getScrollStatus(), pos = scrollStatus.position;
+    if (pos > config.maxPos) {
+      return;
     }
 
-    // site desc
-    if (pos < 46) {
-      var offset = (pos - 10.5) * multiple;
-      if (isPageUp) {
-        if (pos > 10) {
-          el.$siteDesc.css('left', el.$siteDesc.data('origin') - offset);
-        }
-      } else if (el.$siteDesc.data('offset') !== 0) {
-        offset = offset < 10 ? 0 : offset;
-        el.$siteDesc.css('left', el.$siteDesc.data('origin') - offset);
+    var $el = config.$el, offset = (pos - config.minPos + 0.5) * this.scroll.MULTIPLE;
+    if (scrollStatus.isPageUp) {
+      if (pos > config.minPos) {
+        $el.css(config.cssProp, ($el.data('origin') || 0) - offset);
       }
-      el.$siteDesc.data('offset', offset);
+    } else if ($el.data('offset') !== 0) {
+      offset = offset < config.minOffset ? 0 : offset; // if offset is less than 10px
+      $el.css(config.cssProp, ($el.data('origin') || 0) - offset);
+    }
+    $el.data('offset', offset);
+  }
+
+  function moveFooter() {
+    var config = {
+      $el: this._el.$foot,
+      cssProp: 'bottom',
+      maxPos: 30,
+      minPos: 19,
+      minOffset: 12
+    };
+
+    move.call(this, config);
+  }
+
+  function moveSiteDesc() {
+    var config = {
+      $el: this._el.$siteDesc,
+      cssProp: 'left',
+      maxPos: 46,
+      minPos: 10,
+      minOffset: 10
+    };
+
+    move.call(this, config);
+  }
+
+  function moveSiteMenu() {
+    var pos = this.getScrollStatus().position;
+    // site-menu is active in [0, 32]
+    if (pos > 32) {
+      return;
     }
 
-    // site title
-    if (pos < 60) {
-      var offset = (pos - 19.5) * multiple;
-      if (isPageUp) {
-        if (pos > 19) {
-          el.$siteTitle.css('left', el.$siteTitle.data('origin') - offset);
-        }
-      } else if (el.$siteDesc.data('offset') !== 0) {
-        // continue moving until the offset reaches to 0
-        offset = offset < 12 ? 0 : offset;
-        el.$siteTitle.css('left', el.$siteTitle.data('origin') - offset);
-      }
-      el.$siteTitle.data('offset', offset);
+    var offset = pos * this.scroll.MULTIPLE;
+    this._el.$siteMenu.css('left', -1 * offset);
+  }
+
+  function moveSiteTitle() {
+    var config = {
+      $el: this._el.$siteTitle,
+      cssProp: 'left',
+      maxPos: 60,
+      minPos: 19,
+      minOffset: 12
+    };
+
+    move.call(this, config);
+  }
+
+  return {
+    events: {
+      'scroll window': 'handleScrolling'
+    },
+
+    selectors: {
+      head: '#home-head',
+      home: '#home',
+      foot: '#home-foot',
+      siteDesc: '#site-desc',
+      siteMenu: '#site-menu',
+      siteTitle: '#site-title'
+    },
+
+    initialize: function() {
+      this.invoke('br.controller.BaseController:initialize');
+      loadShow.call(this);
+    },
+
+    handleScrolling: function() {
+      this.initScrollStatus();
+
+      var status = this.getScrollStatus();
+      //DEBUG
+      //console.log(status);
+
+      moveSiteMenu.call(this);
+      moveSiteDesc.call(this);
+      moveSiteTitle.call(this);
+      moveFooter.call(this);
     }
   }
 });
 
-TM.declare('br.controller.ShopController').inherit('br.controller.BaseController').extend({
-  events: {
-    'scroll window': 'handleScrolling'
-  },
+TM.declare('br.controller.ShopController').inherit('br.controller.BaseController').extend(function() {
+  function getPropValue(config, pos) {
+    var $el = config.$el, offset = (pos - config.minPos + 0.5) * this.scroll.MULTIPLE,
+      startPoint = $el.data('startPoint'), endPoint = $el.data('endPoint'),
+      propValue = startPoint + offset;
 
-  rootNode: '#shop',
+    if (endPoint > startPoint) {
+      propValue = propValue > endPoint ? endPoint : propValue;
+      propValue = propValue < startPoint ? startPoint : propValue;
+    } else {
+      propValue = propValue > startPoint ? startPoint : propValue;
+      propValue = propValue < endPoint ? endPoint : propValue;
+    }
+    return propValue;
+  }
 
-  selectors: {
-    firstCol: '#shop-col-first',
-    secondCol: '#shop-col-second'
-  },
+  function isAtPosition($el, isPageUp) {
+    if (isPageUp) {
+      return $el.data('propValue') === $el.data('endPoint');
+    } else {
+      return $el.data('propValue') === $el.data('startPoint');
+    }
+  }
 
-  /* shop section shows [500, 1000]*/
-  handleScrolling: function() {
-    var scrollTop = $(window).scrollTop();
+  /*
+  * shop section moves from right side
+  * */
+  function moveShopSection() {
+    var config = {
+      $el: this._$root,
+      cssProp: 'right',
+      maxPos: 82,
+      minPos: 30,
+      minOffset: 10
+    };
 
-  },
+    move.call(this, config);
+  }
 
-  slideIn: function() {
+  function move(config) {
+    var scrollStatus = this.getScrollStatus(), pos = scrollStatus.position;
+    if (pos > config.maxPos) {
+      return;
+    }
 
-  },
+    var $el = config.$el, isPageUp = scrollStatus.isPageUp;
+    if (isAtPosition($el, scrollStatus.isPageUp)) {
+      return;
+    }
 
-  slideOut: function() {
+    var propValue = getPropValue.call(this, config, pos);
+    if (propValue === $el.data('startPoint')) {
+      $el.hide();
+    } else {
+      $el.show();
+    }
 
+    if (isPageUp) { // page goes up
+      if (pos > config.minPos) {
+        $el.css(config.cssProp, propValue);
+      }
+    } else { // page goes down
+      $el.css(config.cssProp, propValue);
+    }
+    $el.data('propValue', propValue);
+  }
+
+  function moveLeftColumn() {
+    var config = {
+      $el: this._el.$leftCol,
+      cssProp: 'left',
+      maxPos: 120,
+      minPos: 82,
+      minOffset: 15
+    };
+
+    move.call(this, config);
+  }
+
+  function moveRightColumn() {
+    var config = {
+      $el: this._el.$rightCol,
+      cssProp: 'right',
+      maxPos: 120,
+      minPos: 82,
+      minOffset: 15
+    };
+
+    move.call(this, config);
+  }
+
+  return {
+    events: {
+      'scroll window': 'handleScrolling'
+    },
+
+    rootNode: '#shop',
+
+    selectors: {
+      leftCol: '#shop-col-first',
+      rightCol: '#shop-col-second',
+      sectionInner: '.br-section-inner'
+    },
+
+    initialize: function() {
+      this.invoke('br.controller.BaseController:initialize');
+
+      var width = $(window).width();
+      this._$root.css({left: 'auto', right: -width}).data({
+        startPoint: -width,
+        endPoint: 0
+      });
+
+      var startPoint = -width / 2;
+      this._el.$leftCol.css('left', startPoint).data({
+        startPoint: startPoint,
+        endPoint: 0
+      });
+
+      this._el.$rightCol.css('right', startPoint).data({
+        startPoint: startPoint,
+        endPoint: 0
+      });
+    },
+
+    handleScrolling: function() {
+      this.initScrollStatus();
+
+      moveShopSection.call(this);
+      moveLeftColumn.call(this);
+      moveRightColumn.call(this);
+    }
   }
 });
 
